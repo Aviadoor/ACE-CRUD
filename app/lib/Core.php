cd93987e000ccd933760b113394879a58fcf468a
Diego <diego.2003.29.12@gmail.com>
2026-04-30 09:07:17 -0500

    Subiendo repositorio ACE - Linux Windows

---
+++ app/lib/Core.php
@@ -0,0 +1,17 @@
+<?php
+class Core{
+
+    public function __construct()
+    {
+
+        $this->getUrl();
+
+    }
+
+    public function getUrl()
+    {
+
+        echo $_GET['url'];
+
+    }
+}
---
+++ public/index.php
@@ -0,0 +1,4 @@
+<?php
+require_once '../app/init.php';
+echo "<h1>Hello World</h1>";
+$init = new Core();
