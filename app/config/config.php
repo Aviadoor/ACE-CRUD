621380b1170289abfb54f718832808d4339efa3d
Diego <diego.2003.29.12@gmail.com>
2026-05-02 09:24:55 -0500

    Dejar de rastrear config.php

--- app/config/config.php
+++ app/config/config.php
@@ -1,12 +1,12 @@
 <?php
 define('APP', dirname(dirname(__FILE__)));
 
-define('URL', 'http://ace.test');
+define('URL', 'http://pruebasdiego');
 
-define('SITENAME', 'ACE-CRUD');
+define('SITENAME', 'pruebasdiego');
 
 //MySQL
-define('DB_HOST', 'ace.test');
+define('DB_HOST', 'pruebasdiego');
 define('DB_USER', 'root');
-define('DB_PASS', 'admin');
+define('DB_PASS', 'd13goMySQL_2912*1306');
 define('DB_NAME', 'ace');
