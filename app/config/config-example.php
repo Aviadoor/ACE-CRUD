4caba6b7a85fcbbab7a9cb2c953cb2d83d73e304
Diego <diego.2003.29.12@gmail.com>
2026-05-03 11:17:12 -0500

    agregando plantilla config.php

---
+++ app/config/config-example.php
@@ -0,0 +1,15 @@
+<?php
+//Colocar los datos correspondientes
+//Renombrar el archivo a config.php
+
+define('APP', dirname(dirname(__FILE__)));
+
+define('URL', '');
+
+define('SITENAME', '');
+
+//MySQL
+define('DB_HOST', '');
+define('DB_USER', '');
+define('DB_PASS', '');
+define('DB_NAME', '');
