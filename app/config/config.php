a6ddf9b5599a2872fe5a56900521fa6af54f795d
Diego <diego.2003.29.12@gmail.com>
2026-05-03 00:01:23 -0500

    Plantilla de app/config/config.php

--- app/config/config.php
+++ app/config/config.php
@@ -1,12 +1,12 @@
 <?php
 define('APP', dirname(dirname(__FILE__)));
 
-define('URL', 'http://pruebasdiego');
+define('URL', ''); //url
 
-define('SITENAME', 'pruebasdiego');
+define('SITENAME', ''); //dominio
 
 //MySQL
-define('DB_HOST', 'pruebasdiego');
-define('DB_USER', 'root');
-define('DB_PASS', 'd13goMySQL_2912*1306');
-define('DB_NAME', 'ace');
+define('DB_HOST', ''); //Host (comunmente:localhost)
+define('DB_USER', ''); //usuario
+define('DB_PASS', ''); //contrasenia BD
+define('DB_NAME', ''); //nombre de la base de datos
